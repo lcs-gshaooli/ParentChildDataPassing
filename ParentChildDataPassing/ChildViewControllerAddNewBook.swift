159f5ed50774911d453725789aa2bb351b666655
Russell Gordon <rgordon@lcs.on.ca>
2020-06-03 15:27:14 -0400

    Defined a child view controller to work with the child view for adding a new book.

---
+++ ParentChildDataPassing/ChildViewControllerAddNewBook.swift
@@ -0,0 +1,24 @@
+//
+//  ChildViewControllerAddNewBook.swift
+//  ParentChildDataPassing
+//
+//  Created by Russell Gordon on 2020-06-03.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewControllerAddNewBook: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
