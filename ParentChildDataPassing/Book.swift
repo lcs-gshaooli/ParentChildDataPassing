19ace63819e3f6debf0be97cbc8f0ff2f4dd595f
Russell Gordon <rgordon@lcs.on.ca>
2020-06-03 15:08:21 -0400

    Added structure to model a book.

---
+++ ParentChildDataPassing/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  ParentChildDataPassing
+//
+//  Created by Russell Gordon on 2020-06-03.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    var title: String
+    var author: String
+    var pageCount: Int
+}
